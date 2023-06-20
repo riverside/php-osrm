426aba1751d9176a17ebe00aca5e3c6cc50d0002
Dimitar Ivanov <biggie@abv.bg>
2023-06-20 21:18:45 +0300

    Update MatchTest.php

--- tests/Service/MatchTest.php
+++ tests/Service/MatchTest.php
@@ -9,8 +9,8 @@ class MatchTest extends TestCase
 {
     public function testMatch()
     {
-        $match = new Match();
-        $response = $match->fetch('13.388860,52.517037;13.397634,52.529407');
+        $matchInst = new Match();
+        $response = $matchInst->fetch('13.388860,52.517037;13.397634,52.529407');
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->isOK());
