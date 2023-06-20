daf147cfbab19f764a5f704a0ab9467b099f6ee4
Dimitar Ivanov <biggie@abv.bg>
2023-06-20 21:41:06 +0300

    Update MatchTest.php

--- tests/Service/MatchTest.php
+++ tests/Service/MatchTest.php
@@ -2,14 +2,17 @@
 namespace OSRM\Tests\Service;
 
 use PHPUnit\Framework\TestCase;
+#use OSRM\Service\Match;
 use OSRM\Response;
 
 class MatchTest extends TestCase
 {
     public function testMatch()
     {
-        $match = new \OSRM\Service\Match();
-        $response = $match->fetch('13.388860,52.517037;13.397634,52.529407');
+        //$match = new Match();
+        //$response = $match->fetch('13.388860,52.517037;13.397634,52.529407');
+        $method = new \ReflectionMethod('\\OSRM\\Service\\Match', 'fetch');
+        $response = $method->invoke(new \OSRM\Service\Match,'13.388860,52.517037;13.397634,52.529407');
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->isOK());
