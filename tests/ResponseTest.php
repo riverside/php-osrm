4b16b733f70a3db2fb045285bc4cf47fc674df05
Dimitar Ivanov <biggie@abv.bg>
2023-06-20 18:22:17 +0300

    Update tests

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -1,10 +1,13 @@
 <?php
-namespace OSRM;
+namespace OSRM\Tests;
 
 use PHPUnit\Framework\TestCase;
+use OSRM\Response;
 
 class ResponseTest extends TestCase
 {
+    protected $data = '{"code":"Ok","routes":[{"legs":[{"steps":[],"summary":"","weight":263.1,"duration":260.2,"distance":1886.3},{"steps":[],"summary":"","weight":370.6,"duration":370.6,"distance":2845.5}],"weight_name":"routability","weight":633.7,"duration":630.8,"distance":4731.8}],"waypoints":[{"hint":"lv4JgHKgmoUXAAAABQAAAAAAAAAgAAAAIXRPQYXNK0AAAAAAcPePQQsAAAADAAAAAAAAABAAAACL-wAA_kvMAKlYIQM8TMwArVghAwAA7wpWZObe","distance":4.231521214,"name":"Friedrichstraße","location":[13.388798,52.517033]},{"hint":"t1jdgVXEiocGAAAACgAAAAAAAAB3AAAAppONQOodwkAAAAAA8TeEQgYAAAAKAAAAAAAAAHcAAACL-wAAfm7MABiJIQOCbswA_4ghAwAAXwVWZObe","distance":2.795148358,"name":"Torstraße","location":[13.39763,52.529432]},{"hint":"1igYgP___38fAAAAUQAAACYAAAAeAAAAeosKQlNOX0IQ7CZCjsMGQh8AAABRAAAAJgAAAB4AAACL-wAASufMAOdwIQNL58wA03AhAwQAvxBWZObe","distance":2.226580806,"name":"Platz der Vereinten Nationen","location":[13.428554,52.523239]}]}';
+
     public function testSuccess()
     {
         $attributes = array(
@@ -13,5 +16,33 @@ class ResponseTest extends TestCase
         foreach ($attributes as $attribute) {
             $this->assertClassHasAttribute($attribute, Response::class);
         }
+    }
+
+    public function testCode()
+    {
+        $response = new Response($this->data);
+
+        $this->assertTrue($response->isOK(), 'Response code should be OK');
+    }
+
+    public function testError()
+    {
+        $response = new Response($this->data);
+
+        $this->assertEmpty($response->getError(), 'Response should not have a error');
+    }
+
+    public function testArray()
+    {
+        $response = new Response($this->data);
+
+        $this->assertIsArray($response->toArray(), 'Response data should be an array');
+    }
+
+    public function testJson()
+    {
+        $response = new Response($this->data);
+
+        $this->assertJson($response->toJson(), 'Response data should ne json');
     }
 }
--- tests/TransportTest.php
+++ tests/TransportTest.php
@@ -1,7 +1,8 @@
 <?php
-namespace OSRM;
+namespace OSRM\Tests;
 
 use PHPUnit\Framework\TestCase;
+use OSRM\Transport;
 
 class TransportTest extends TestCase
 {
@@ -18,5 +19,19 @@ class TransportTest extends TestCase
         foreach ($attributes as $attribute) {
             $this->assertClassHasAttribute($attribute, Transport::class);
         }
+    }
+
+    public function testMain()
+    {
+        $transport = new Transport();
+
+        $this->assertInstanceOf(Transport::class, $transport->setConnectTimeout(30));
+        $this->assertInstanceOf(Transport::class, $transport->setTimeout(30));
+        $this->assertInstanceOf(Transport::class, $transport->setSslVerifyPeer(true));
+        $this->assertInstanceOf(Transport::class, $transport->setUserAgent('Chrome'));
+        $this->assertInstanceOf(Transport::class, $transport->request('route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'));
+        $this->assertIsInt($transport->getHttpCode());
+        $this->assertSame(200, $transport->getHttpCode());
+        $this->assertNotEmpty($transport->getResponse());
     }
 }
