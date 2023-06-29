2fb83337b8f88db9c1e91681a6df996750079bf8
Dimitar Ivanov <biggie@abv.bg>
2023-06-29 14:34:52 +0300

    Create isochrones.php

---
+++ examples/isochrones.php
@@ -0,0 +1,28 @@
+<?php
+include __DIR__ . '/../src/autoload.php';
+
+$inst = new \OSRM\Isochrones\OpenRouteService();
+
+$inst->setApiKey('{api_key}');
+
+try {
+    $response = $inst
+        ->setLocations([[8.681495,49.41461],[8.686507,49.41943]])
+        ->setRange([300,200])
+        ->fetch();
+    if ($response->isOK())
+    {
+        echo '<pre>';
+        print_r($response->getFeatures());
+        print_r($response->getBbox());
+        print_r($response->getMetadata());
+        var_dump($response->getType());
+        print_r($response->toArray());
+    } else {
+        echo 'Isochrone not found.';
+    }
+} catch (\OSRM\Exception $e) {
+    echo $e->getMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
